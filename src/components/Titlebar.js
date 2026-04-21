f0183e8054228e79dce49759aec62cc251700f1c
ErTavo <66528756+ErTavo@users.noreply.github.com>
2026-04-21 14:29:26 -0600

    icon update

--- src/components/Titlebar.js
+++ src/components/Titlebar.js
@@ -1,17 +1,11 @@
-import Image from "next/image";
 import styles from "@/styles/Titlebar.module.css";
 import { OPTIONS, TITLE, BUTTONS } from "@/constants/titlebar";
+import VSCodeIcon from "@/svg/VSCodeicon";
 
 const Titlebar = () => {
   return (
     <section className={styles.titlebar}>
-      <Image 
-        src="https://cdn.simpleicons.org/visualstudiocode/007ACC"
-        alt="VSCode Icon"
-        width={15}
-        height={15}
-        className={styles.icon}
-      />
+      <VSCodeIcon className={styles.icon} />
       <div className={styles.items}>
         {OPTIONS.map((option,) => (
           <p key={`option_titlebar_${option}`}>
